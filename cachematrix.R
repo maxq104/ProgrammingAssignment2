f45b1c4d1393276231f2044f877e1c5e192e1503
maxq104 <maxq104@comcast.net>
2015-05-16 10:02:13 -0500

    Updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment Number 2
+## Jim Christensen, May, 2015
 
-## Write a short comment describing this function
+## The makeCacheMatrix function makes use of R lexical scoping to cache the inverse of a matrix
+## so it does not need to be re-computed if there are no changes to the matrix.
+## This function creates a special "matrix" object that can cache its inverse.
+
+## the cacheSolve function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix. If the inverse has already been calculated (and the matrix has not 
+## changed), then cacheSolve should retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
