92175653be7805bc998be8e28ac6115d249e5b29
maxq104 <maxq104@comcast.net>
2015-05-17 20:32:31 -0500

    Tweaked code, added more comments, added functions to test the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## R Programming Assignment Number 2
-## Jim Christensen, May, 2015
+## Jim Christensen, May 17, 2015
 
 ## The makeCacheMatrix function makes use of R lexical scoping to cache the inverse of a matrix
 ## so it does not need to be re-computed if there are no changes to the input matrix.
@@ -12,18 +12,20 @@
 makeCacheMatrix <- function(x = matrix()) {
                         ## define a list of functions to set and get the input matrix (x)
                         ## and to set and get the inverse of the matrix (im)
+
   im <- NULL
-  setMatrix <- function(y) {
-    x <<- y
+  
+  setMatrix <- function(y) {            ## this function wil save the input matrix in the
+    x <<- y                             ## parent environment
     im <<- NULL
   }
-  getMatrix <- function() {
+  getMatrix <- function() {             ## this function will return the input matrix
     x
   }
-  setInverse <- function(inverse) {
-    im <<- inverse
+  setInverse <- function(inverse) {     ## this function will cache the inverted matrix in the
+    im <<- inverse                      ## parent environment
   }
-  getInverse <- function() {
+  getInverse <- function() {            ## this function will return the cached inverted matrix
     im
   }
   
@@ -43,8 +45,38 @@ cacheSolve <- function(x, ...) {
     return(im)
   }
   matrix <- x$getMatrix()                                 ## inverse not computed yet, get the input matrix
-  im <- solve(matrix)                                     ## solve (invert) the matrix
+  im <- solve(matrix)                                     ## solve (invert) the input matrix
+  message("solving for the inverse of the matrix")
   x$setInverse(im)                                        ## then cache the result for later
   im
     
+}
+
+go <- function()  {                                       ## create a function to test the code
+  
+  mymat <- matrix(c(2,5,3,7,3,8,4,7,2),3,3)               ## create a test matrix
+  
+  mm <- makeCacheMatrix(mymat)
+  
+  print(cacheSolve(mm))                   ## the first time cacheSolve is called, the inverse is computed
+  print(cacheSolve(mm))                   ## the second and third time, the inverse is recalled from cache
+  print(cacheSolve(mm))
+  
+}
+
+go2 <- function()  {                                      ## create a function to test the code
+  
+  mm <- makeCacheMatrix()                                 ## create the special matrix (function list)
+  
+  mymat <- matrix(c(2,5,3,7,3,8,4,7,2),3,3)               ## create a test matrix
+  
+  mm$setMatrix(mymat)                     ## set the value of the input matrix
+  xx <- mm$getMatrix()                    ## retrieve the input matrix
+  message("Input matrix")
+  print(xx)                               ## check the return value of the matrix
+  
+  print(cacheSolve(mm))                   ## the first time cacheSolve is called, the inverse is computed
+  print(cacheSolve(mm))                   ## the second and third time, the inverse is recalled from cache
+  print(cacheSolve(mm))
+  
 }
